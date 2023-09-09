fbbc7a56e389c5d3fc0d5b4b484dad9cd8fed718
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2023-09-09 16:29:39 +0200

    Add reward bot

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 import sleepy from './icons/sleepy_bot.png';
 import awake from './icons/awake_bot.png';
 import happy from './icons/happy_bot.png';
+import red from './icons/red.png';
+import blue from './icons/blue.png';
+import yellow from './icons/yellow.png';
+import pink from './icons/pink.png';
 import * as VIAM from '@viamrobotics/sdk';
 import {useState} from "react";
+import './ghosts.css'
 
 function App() {
   const [robotCharacter, setRobotCharacter]: string = useState(sleepy);
@@ -35,9 +40,19 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        <div className="robot-character"><img height="100px" src={robotCharacter} alt="Robot"></img></div>
-        <div className="clean-button" onClick={handleCleanButton}>Clean</div>
+      <div className="website-title">. . . Reward-E . . .</div>
+      <div className="robot-container">
+        <div className="robot-character">
+          <img height="100px" src={robotCharacter} alt="Robot"/>
+        </div>
+        <div className="button focus-button" onClick={handleCleanButton}>Focus Time</div>
+      </div>
+      <div className="spacer"></div>
+      <div className="ghost-container">
+        <img src={red} className="ghost red-ghost" alt="Red ghost"/>
+        <img src={blue} className="ghost blue-ghost" alt="Blue ghost"/>
+        <img src={pink} className="ghost pink-ghost" alt="Pink ghost"/>
+        <img src={yellow} className="ghost yellow-ghost" alt="Yellow ghost"/>
       </div>
     </>
   );
